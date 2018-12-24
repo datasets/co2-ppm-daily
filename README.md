dd875ca2f93a34c7dc925e32f1a85e7e07a8f59b
Svetozar Stojkovic <svetozar.cvele.stojkovic@gmail.com>
2018-12-24 19:14:02 +0100

    [l][init] * added script/main.py; * added data/co2-ppm-daily.csv; * added datapackage.json; added README.md; * added requirements.txt

---
+++ README.md
@@ -0,0 +1,27 @@
+## CO2 emission changing on daily level
+
+This dataset was created using data from [Earth System Research Laboratory](https://www.esrl.noaa.gov/).
+
+### Preparation
+
+To generate output file you should only run the script on location:
+`scripts/main.py`
+
+### Data
+Output file is located in: `data/co2-ppm-daily.csv`
+
+
+## License
+
+### ODC-PDDL-1.0
+
+This Data Package is made available under the Public Domain Dedication and License v1.0 whose full text can be found at: http://www.opendatacommons.org/licenses/pddl/1.0/
+
+### Notes
+
+The [terms of use][gmd] of the source dataset list three specific restrictions on public use of these data:
+
+> The information on government servers are in the public domain, unless specifically annotated otherwise, and may be used freely by the public so long as you do not 1) claim it is your own (e.g. by claiming copyright for NOAA information – see next paragraph), 2) use it in a manner that implies an endorsement or affiliation with NOAA, or 3) modify it in content and then present it as official government material.[*][gmd]
+
+[ccgg-trends]: http://www.esrl.noaa.gov/gmd/ccgg/trends/index.html
+[gmd]: http://www.esrl.noaa.gov/gmd/about/disclaimer.html
---
+++ scripts/main.py
@@ -0,0 +1,56 @@
+import urllib.request
+from dataflows import PackageWrapper, ResourceWrapper, Flow, dump_to_path, printer
+
+
+def get_data():
+    header = True
+    resource = urllib.request.urlopen(
+        "ftp://aftp.cmdl.noaa.gov/data/trace_gases/co2/in-situ/surface/mlo/co2_mlo_surface-insitu_1_ccgg_DailyData.txt")
+    for row in resource.readlines():
+        usable_row = row.decode('utf-8').replace('\n', '')
+        if not usable_row.startswith('#'):
+            parts = usable_row.split(' ')
+            if header:
+                header = False
+            else:
+                date = parts[1] + '-' + f"{int(parts[2]):02d}" + '-' + f"{int(parts[3]):02d}"
+                value = '' if '-999.99' in parts[7] else parts[7]
+                yield dict(
+                    date=date,
+                    value=value
+                )
+
+    header = True
+    resource = urllib.request.urlopen('https://www.esrl.noaa.gov/gmd/webdata/ccgg/trends/co2_mlo_weekly.csv')
+    for row in resource.readlines():
+        usable_row = row.decode('utf-8').replace('\n', '')
+        parts = usable_row.split(',')
+        if header:
+            header = False
+        else:
+            date = parts[0]
+            value = parts[1]
+            if "2017-" not in date:
+                yield dict(
+                    date=date,
+                    value=value
+                )
+
+
+def change_path(package: PackageWrapper):
+    package.pkg.descriptor['resources'][0]['path'] = 'data/co2-ppm-daily.csv'
+    package.pkg.descriptor['resources'][0]['name'] = 'co2-ppm-daily'
+
+    yield package.pkg
+    res_iter = iter(package)
+    first: ResourceWrapper = next(res_iter)
+    yield first.it
+    yield from package
+
+
+Flow(get_data(),
+     change_path,
+     dump_to_path('../'),
+     printer()).process()
+
+
