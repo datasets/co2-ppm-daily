bfb2c44f2fc96e5dba1ba3d86016bf419b3c70e7
Svetozar Stojkovic <svetozar.cvele.stojkovic@gmail.com>
2019-06-09 00:06:38 +0200

    [script, data, datapackage][m] * changed logic so that only new source is being read

--- co2-ppm-daily-flow.py
+++ co2-ppm-daily-flow.py
@@ -1,10 +1,11 @@
 import datetime
+import os
 import urllib.request
 
 from dataflows import PackageWrapper, ResourceWrapper, Flow, dump_to_path
 
 
-def get_data():
+def get_all_data():
     all_years = {}
     # first source containing info from 01.01.1958 to 2004
     header = True
@@ -41,6 +42,30 @@ def get_data():
                 if value is not None:
                     date = datetime.datetime.strptime(date, '%Y-%m-%d')
                     all_years[date] = value
+
+    all_years = sorted(all_years.items())
+    for date, value in all_years:
+        yield dict(
+            date=date.strftime("%Y-%m-%d"),
+            value=value
+        )
+
+
+def get_only_2018_and_on():
+    all_years = {}
+
+    header = True
+    for row in open('data/co2-ppm-daily.csv'):
+        usable_row = row.replace('\n', '')
+        parts = usable_row.split(',')
+        if header:
+            header = False
+        else:
+            date = parts[0]
+            value = parts[1]
+            if value is not '':
+                date = datetime.datetime.strptime(date, '%Y-%m-%d')
+                all_years[date] = value
 
     # third source containing info from 2017 until today
     header = True
@@ -209,7 +234,10 @@ def change_path(package: PackageWrapper):
     yield from package
 
 
-co2_ppm_daily = Flow(get_data(), change_path, dump_to_path())
+if os.path.exists('data/co2-ppm-daily.csv'):
+    co2_ppm_daily = Flow(get_only_2018_and_on(), change_path, dump_to_path())
+else:
+    co2_ppm_daily = Flow(get_all_data(), change_path, dump_to_path())
 
 
 if __name__ == '__main__':
